22d9909972bb39f301be96c33b6fce8c911e0a65
Benjamin Vellacott <bmvellacott@gmail.com>
2016-02-07 01:37:14 +0000

    formed the basis for actions and undo/redo

--- smack-actions.js
+++ smack-actions.js
@@ -3,9 +3,13 @@ if(!Smack)
 Smack.actions = {};
 
 Smack.actions = (function(){
-	var stack = {};
+	var stack = [];
 	return {
 		undo : function() { throw 'undo hasn\'t been implemented'; },
 		redo : function() { throw 'redo hasn\'t been implemented'; },
+		newContentEdit : {
+			doo : function() { throw 'newContentAdd.doo hasn\'t been implemented'; },
+			undoo : function() { throw 'newContentAdd.undoo hasn\'t been implemented'; },
+		},
 	}
 })();
