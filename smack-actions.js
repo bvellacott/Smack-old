355b02dcb320625aa913efa2012cd178c28d0b3d
Benjamin Vellacott <bmvellacott@gmail.com>
2016-02-07 01:15:08 +0000

    added undo/redo interception

--- smack-actions.js
+++ smack-actions.js
@@ -1,3 +1,11 @@
 if(!Smack)
 	throw 'Smack is required';
 Smack.actions = {};
+
+Smack.actions = (function(){
+	var stack = {};
+	return {
+		undo : function() { throw 'undo hasn\'t been implemented'; },
+		redo : function() { throw 'redo hasn\'t been implemented'; },
+	}
+})();
